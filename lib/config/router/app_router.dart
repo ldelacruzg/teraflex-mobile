8e34f810abfa55a46eb5680c230d112353878589
Luis De La Cruz <ldelacruzg@uteq.edu.ec>
2024-01-05 01:52:32 -0500

    fix: part of the prev commit

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,13 +1,15 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:teraflex_mobile/features/auth/ui/screens/login_screen.dart';
 import 'package:teraflex_mobile/features/home/ui/screens/home_screen.dart';
 import 'package:teraflex_mobile/features/settings/ui/screens/profile/profile_view.dart';
 import 'package:teraflex_mobile/features/settings/ui/screens/security/security_view.dart';
 import 'package:teraflex_mobile/features/settings/ui/screens/setting_screen.dart';
+import 'package:teraflex_mobile/features/treatments/ui/screens/treatment_tasks_screen.dart';
 import 'package:teraflex_mobile/features/welcome_messages/ui/screens/welcome_messages_screen.dart';
 
 final appRouter = GoRouter(
-  initialLocation: '/',
+  initialLocation: '/home',
   routes: [
     GoRoute(
       path: '/',
@@ -24,6 +26,22 @@ final appRouter = GoRouter(
       path: '/home',
       name: HomeScreen.name,
       builder: (context, state) => const HomeScreen(),
+      routes: [
+        GoRoute(
+          path: 'treatments',
+          builder: (context, state) => const SizedBox(),
+          routes: [
+            GoRoute(
+              path: ':id',
+              name: TreatmentTasksScreen.name,
+              builder: (context, state) {
+                final treatmentId = state.pathParameters['id'] ?? 'no-id';
+                return TreatmentTasksScreen(treatmentId: treatmentId);
+              },
+            ),
+          ],
+        ),
+      ],
     ),
     GoRoute(
       path: '/settings',
