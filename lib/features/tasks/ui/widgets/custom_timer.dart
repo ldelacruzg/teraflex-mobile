d20c7e376254654d61121b8eb76cfb8a4a641dc9
Luis De La Cruz <ldelacruzg@uteq.edu.ec>
2024-01-18 23:14:49 -0500

    fix: initialize the timer by default

---
+++ lib/features/tasks/ui/widgets/custom_timer.dart
@@ -0,0 +1,120 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class CustomTimer extends StatefulWidget {
+  final Duration duration;
+  final void Function()? onStart;
+  final void Function()? onFinished;
+  final bool autoStart;
+
+  const CustomTimer({
+    super.key,
+    required this.duration,
+    this.onStart,
+    this.onFinished,
+    this.autoStart = true,
+  });
+
+  @override
+  State<CustomTimer> createState() => _CustomTimerState();
+}
+
+class _CustomTimerState extends State<CustomTimer> {
+  late Timer _timer;
+  late Duration _start;
+  Duration _countdown = const Duration(seconds: 5);
+  bool _isRunning = false;
+  bool _isCountdown = false;
+
+  @override
+  void initState() {
+    _start = widget.duration;
+    _timer = Timer(Duration.zero, () {});
+
+    if (widget.autoStart) {
+      _startTimer();
+    }
+
+    super.initState();
+  }
+
+  void _startTimer() {
+    _isRunning = true;
+    widget.onStart?.call();
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (_start.inSeconds == 0) {
+        setState(() {
+          _start = widget.duration;
+        });
+        widget.onFinished?.call();
+      } else {
+        setState(() {
+          _start -= const Duration(seconds: 1);
+        });
+      }
+    });
+  }
+
+  void _startCountdown() {
+    _isCountdown = true;
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (_countdown.inSeconds == 0) {
+        setState(() {
+          timer.cancel();
+        });
+        _startTimer();
+      } else {
+        setState(() {
+          _countdown -= const Duration(seconds: 1);
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!_isRunning) {
+      if (_isCountdown) {
+        return Column(
+          children: [
+            const Text(
+              'Comenzamos en',
+              style: TextStyle(fontSize: 16),
+            ),
+            Text(
+              "${_countdown.inSeconds}",
+              style: const TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        );
+      }
+
+      return IconButton.filled(
+        onPressed: () {
+          setState(() {
+            _startCountdown();
+          });
+        },
+        icon: const Icon(Icons.play_arrow_rounded, size: 40),
+      );
+    }
+
+    return Text(
+      "${_start.inMinutes}:${(_start.inSeconds % 60).toString().padLeft(2, '0')}",
+      style: const TextStyle(
+        fontSize: 50,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+}
